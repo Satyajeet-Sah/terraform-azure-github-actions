4f0df57088b29d59564a5a69d68d4c915e91cf23
Satyajeet <satyajeet@hjinfotech.com>
2026-09-12 11:01:20 +0530

    Did terraform fmt to structure the code

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-variable rg_data_name {
-    type = string
+variable "rg_data_name" {
+  type = string
 }
 
-variable rg_data_location {
-    type = string
+variable "rg_data_location" {
+  type = string
 }
